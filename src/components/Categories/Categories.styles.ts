c578f9cab28eb020f52d83dd934ca1636f637a3b
Reinaldo Salla <reinaldo.salla@compasso.com.br>
2020-07-30 10:35:15 -0300

    Refactored the categories, problems with max-width in flexbox and realized that it' s a good ideia to create RoundedIcon component

--- src/components/Categories/Categories.styles.ts
+++ src/components/Categories/Categories.styles.ts
@@ -1,131 +1,252 @@
+// import styled from 'styled-components';
+// import { IoLogoJavascript } from 'react-icons/io';
+// import { GiShieldEchoes } from 'react-icons/gi';
+
+// const titleSize = '2em';
+
+// const icon = `
+//   padding: 0.5em;
+//   margin-top: 1em;
+//   height: 2em;
+//   width: 2em;
+//   border-radius: 50%;
+//   border: 1px solid black;
+// `;
+
+// const tmpIcon = `
+//   padding: 0.5em;
+//   margin-top: 1em;
+//   font-size: 1.5em;
+//   border-radius: 25%;
+//   border: 1px solid black;
+// `;
+
+// const CategoriesContainer = styled.div`
+//   padding: 0.5em;
+//   margin: 100px 50px 0 50px;
+//   /* border: 1px solid red; */
+// `;
+
+// const CategoriesItemContainer = styled.div`
+//   padding: 0.5em;
+//   margin: 3em 0.5em 3em 0.5em;
+//   border: 1px solid blue;
+//   display: flex;
+//   flex-direction: column;
+// `;
+
+// const CategoriesTitleSvgContainer = styled.div`
+//   /* margin: 0;
+//   padding: 0; */
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   /* border: 1px solid black; */
+// `; 
+
+// const CategoriesJavaScriptSvg = styled(IoLogoJavascript)`${icon}`;
+
+// const CategoriesTypeScriptSvg = styled(GiShieldEchoes)`${icon}`;
+
+// const CategoriesJavaScriptTmpSvg = styled.span`${tmpIcon}`;
+
+// const CategoriesItemTitleName = styled.span`
+//   /* padding: 0.25em; */
+//   /* margin: 0.25em; */
+//   margin-top: 0.5em;
+//   font-weight: 700;
+//   font-size: ${titleSize};
+//   text-align: center;
+//   /* border: 1px solid green; */
+// `;
+
+// const CategoriesItemTitleDescription = styled.span`
+//   /* padding: 0.25rem; */
+//   margin: 1em;
+//   font-weight: 700;
+//   font-size: 1.5em;
+//   text-align: center;
+//   /* border: 1px solid gray; */
+// `;
+
+// const CategoriesItemContentsContainer = styled.div`
+//   /* padding: 0.5em; */
+//   /* margin: 0.25em; */
+//   display: flex;
+//   /* flex-wrap: wrap; */
+//   flex-direction: column;
+//   justify-content: center;
+//   /* align-items: center; */
+//   /* border: 1px solid orange; */
+// `;
+
+// const CategoriesItemContentsItem = styled.div`
+//   padding: 0.5em;
+//   margin: 0.5em;
+//   /* max-width: 1000px; */
+//   /* height: 200px; */
+//   min-height: 150px;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   justify-content: center;
+//   /* flex: 10em; */
+//   /* font-size: 1.25em; */
+//   /* background: yellow; */
+//   border: 1px solid black;
+// `;
+
+// const CategoriesContentsTitle = styled.span`
+//   /* padding: 0.25em;
+//   margin: 0.25em; */
+//   margin-top: 0.5em;
+//   margin-bottom: 1em;
+//   font-size: 1.5em;
+//   text-align: center;
+//   /* border: 1px solid black; */
+// `;
+
+// const CategoriesContentsDescription = styled.span`
+//   /* padding: 0.25em;
+//   margin: 0.25em; */
+//   font-size: 1.25em;
+//   text-align: center;
+//   margin-bottom: 1em;
+//   /* border: 1px solid black; */
+// `;
+
+// export {
+//   CategoriesContainer,
+//   CategoriesItemContainer,
+//   CategoriesTitleSvgContainer,
+//   CategoriesJavaScriptSvg,
+//   CategoriesJavaScriptTmpSvg,
+//   CategoriesTypeScriptSvg,
+//   CategoriesItemTitleName,
+//   CategoriesItemTitleDescription,
+//   CategoriesItemContentsContainer,
+//   CategoriesItemContentsItem,
+//   CategoriesContentsTitle,
+//   CategoriesContentsDescription
+// };
+
 import styled from 'styled-components';
 import { IoLogoJavascript } from 'react-icons/io';
 import { GiShieldEchoes } from 'react-icons/gi';
 
-const titleSize = '2em';
-
-const icon = `
-  padding: 0.5em;
-  margin-top: 1em;
-  height: 2em;
-  width: 2em;
-  border-radius: 50%;
-  border: 1px solid black;
-`;
-
-const tmpIcon = `
-  padding: 0.5em;
-  margin-top: 1em;
-  font-size: 1.5em;
-  border-radius: 25%;
-  border: 1px solid black;
-`;
-
 const CategoriesContainer = styled.div`
   padding: 0.5em;
-  margin: 100px 50px 0 50px;
-  /* border: 1px solid red; */
+  margin: 100px 50px 100px 50px;
+  border: 1px solid black;
 `;
 
 const CategoriesItemContainer = styled.div`
-  padding: 0.5em;
-  margin: 3em 0.5em 3em 0.5em;
-  border: 1px solid blue;
   display: flex;
   flex-direction: column;
-`;
-
-const CategoriesTitleSvgContainer = styled.div`
-  /* margin: 0;
-  padding: 0; */
-  display: flex;
   align-items: center;
-  justify-content: center;
-  /* border: 1px solid black; */
-`; 
-
-const CategoriesJavaScriptSvg = styled(IoLogoJavascript)`${icon}`;
-
-const CategoriesTypeScriptSvg = styled(GiShieldEchoes)`${icon}`;
-
-const CategoriesJavaScriptTmpSvg = styled.span`${tmpIcon}`;
-
-const CategoriesItemTitleName = styled.span`
-  /* padding: 0.25em; */
-  /* margin: 0.25em; */
-  margin-top: 0.5em;
-  font-weight: 700;
-  font-size: ${titleSize};
-  text-align: center;
-  /* border: 1px solid green; */
-`;
-
-const CategoriesItemTitleDescription = styled.span`
-  /* padding: 0.25rem; */
-  margin: 1em;
-  font-weight: 700;
-  font-size: 1.5em;
-  text-align: center;
-  /* border: 1px solid gray; */
-`;
-
-const CategoriesItemContentsContainer = styled.div`
-  /* padding: 0.5em; */
-  /* margin: 0.25em; */
-  display: flex;
-  /* flex-wrap: wrap; */
-  flex-direction: column;
-  justify-content: center;
-  /* align-items: center; */
-  /* border: 1px solid orange; */
-`;
-
-const CategoriesItemContentsItem = styled.div`
   padding: 0.5em;
   margin: 0.5em;
-  /* max-width: 1000px; */
-  /* height: 200px; */
-  min-height: 150px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
   justify-content: center;
-  /* flex: 10em; */
-  /* font-size: 1.25em; */
-  /* background: yellow; */
   border: 1px solid black;
 `;
 
-const CategoriesContentsTitle = styled.span`
-  /* padding: 0.25em;
-  margin: 0.25em; */
-  margin-top: 0.5em;
-  margin-bottom: 1em;
-  font-size: 1.5em;
+const CategoriesIconContainer = styled.div`
+  margin: 0.5em;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  /* background: orange; */
+  /* color: green; */
+  border: 1px solid black;
+  border-radius: 50%;
+`;
+
+const CategoriesBaseIcon = `
+  margin: 10px;
+  height: 2.5em;
+  width: 2.5em;
+`;
+
+const CategoriesJavaScriptIcon = styled(IoLogoJavascript)`
+  ${CategoriesBaseIcon}
+`;
+
+const CategoriesShieldEchoesIcon = styled(GiShieldEchoes)`
+  ${CategoriesBaseIcon}
+`;
+
+const CategoriesTitleText = styled.span`
+  /* padding: 0.5em; */
+  margin-bottom: 0.5em;
+  font-size: 2em;
+  font-weight: 700;
   text-align: center;
   /* border: 1px solid black; */
 `;
 
-const CategoriesContentsDescription = styled.span`
-  /* padding: 0.25em;
-  margin: 0.25em; */
+const CategoriesDescriptionText = styled.span`
+  margin-bottom: 0.5em;
+  font-size: 1.5em;
+  font-weight: 700;
+  text-align: center;
+  /* border: 1px solid black; */
+`;
+
+const CategoriesContentsContainer = styled.div`
+  /* margin: 0.5em;
+  padding: 0.5em;
+  border: 1px solid black; */
+`;
+
+const CategoriesContentItemContainer = styled.div`
+  margin: 0.5em;
+  padding: 0.5em;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  max-width: 600px;
+  border: 1px solid black;
+`;
+
+const CategoriesContentTitleText = styled.span`
+  /* flex: auto; */
+  /* margin: 0.5em; */
+  /* padding: 0.5em; */
+  font-size: 1.5em;
+  text-align: center;
+
+  /* Does't work */
+  /* min-width: 600px;*/ 
+
+  /* Works */
+  min-width: 600px; 
+
+
+  /* border: 1px solid black; */
+`;  
+
+const CategoriesContentDescriptionText = styled.span`
+  /* flex: auto; */
+  /* margin: 0.5em; */
+  /* padding: 0.5em; */
   font-size: 1.25em;
   text-align: center;
-  margin-bottom: 1em;
+  max-width: 600px;
   /* border: 1px solid black; */
 `;
 
 export {
   CategoriesContainer,
   CategoriesItemContainer,
-  CategoriesTitleSvgContainer,
-  CategoriesJavaScriptSvg,
-  CategoriesJavaScriptTmpSvg,
-  CategoriesTypeScriptSvg,
-  CategoriesItemTitleName,
-  CategoriesItemTitleDescription,
-  CategoriesItemContentsContainer,
-  CategoriesItemContentsItem,
-  CategoriesContentsTitle,
-  CategoriesContentsDescription
+  CategoriesIconContainer,
+  CategoriesJavaScriptIcon,
+  CategoriesShieldEchoesIcon,
+  CategoriesTitleText,
+  CategoriesDescriptionText,
+  CategoriesContentsContainer,
+  CategoriesContentItemContainer,
+  CategoriesContentTitleText,
+  CategoriesContentDescriptionText
 };
+
--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -1,138 +1,205 @@
-import React from 'react';
+// import React from 'react';
+// import {
+//   CategoriesContainer,
+//   CategoriesItemContainer,
+//   CategoriesTitleSvgContainer,
+//   CategoriesJavaScriptSvg,
+//   CategoriesJavaScriptTmpSvg,
+//   CategoriesTypeScriptSvg,
+//   CategoriesItemTitleName,
+//   CategoriesItemTitleDescription,
+//   CategoriesItemContentsContainer,
+//   CategoriesItemContentsItem,
+//   CategoriesContentsTitle,
+//   CategoriesContentsDescription
+// } from './Categories.styles';
+// // import contents from '../../data/contents';
+// import { IoLogoJavascript } from 'react-icons/io';
+
+// const topics = [
+//   {
+//     title: 'JavaScript',
+//     icon: <CategoriesJavaScriptSvg/>,
+//     description: 'Curated tutorials from basic to advanced',
+//     contents: [
+//       { title: 'Data Structures', description: 'Understanting JavaScript data types, such as booleans, strings, numbers, nulls, arrays and objects'},
+//       { title: 'Functions', description: 'Learning how to use function declarations, function expressions and arrow functions'},
+// 			{ title: 'Conditionals', description: 'Usings if-else, switch and the ternary operator'},
+// 			{ title: 'Loops', description: 'Iterating over sequences by repetition or by traversing an iterable' },
+// 			{ title: 'Map', description: 'Processing/Mutating each element of an array according to a certain pattern' },
+// 			{ title: 'Filter', description: 'Removing elements of an array according to a certain pattern'},
+//       { title: 'Reduce', description: 'Processing/Mutating each element of an array and generating an accumulator'},
+//       { title: 'Try Catch', description: 'Dealing with erros and exceptions in JavaScript' },
+//       { title: 'Closures', description: 'Holding the environment' },
+//       { title: 'Generators', description: 'Producing sequential items in steps and gaining performance' },
+//       { title: 'Callbacks', description: 'Understanding one of the most fundamental concept from asyncronous programming in JavaScript' },
+// 			{ title: 'Promises', description: 'Making async calls and avoiding callback hell'},
+// 			{ title: 'Async Await', description: 'Syntatic sugar for Generator plus Promises'}
+//     ]
+//   },
+//   {
+//     title: 'TypeScript',
+//     icon: <CategoriesTypeScriptSvg />,
+//     description: 'Fundamentals for extending JavaScript',
+//     contents: [
+// 			{ title: 'Types', description: 'Statically type check JavaScript code' },
+// 			{ title: 'Interfaces', description: 'Creating a blueprint for data consisting of key-value pairs' },
+//       { title: 'Generics', description: 'Allowing the usage of multiple types' },
+//       { title: 'Decorators', description: 'Injecting extra functionality and metadata' }
+//     ]
+//   },
+//   {
+//     title: 'React',
+//     icon: <CategoriesJavaScriptSvg />,
+//     description: 'Concepts from React, focusing on function components and React Hooks',
+//     contents: [
+//       { title: 'Rendering', description: 'Rendering on the browser with JXS' },
+//       { title: 'Styling', description: 'Creating a fully component oriented app with JSS' },
+// 			{ title: 'State', description: 'State management and re-rendering components' },
+// 			{ title: 'Props', description: 'Passing data from parent component to child component' },
+//       { title: 'Side Effects', description: 'Dealing with side effects on the component lifecycle' },
+//       { title: 'Context', description: 'Creating global variables and methods' },
+//       { title: 'Refs', description: 'An escape hatch from react state management' },
+//       { title: 'Memoization', description: 'Improving performance by memoizing data and methods' },
+//       { title: 'Routing', description: 'Creating Single Page Apps with react-router' },
+//       { title: 'Plots', description: 'Creating plots in react with ???' },
+//       { title: 'Animation', description: 'Controlling animations with react-spring' },
+//       { title: 'Graphics', description: 'Usage of 3D graphics with three.js and react-three-fiber' },
+//       { title: 'Videos', description: 'Manipulating videos in react with ???' },
+//     ]
+//   },
+//   {
+//     title: 'Backend',
+//     icon: <CategoriesJavaScriptSvg />,
+//     description: 'Extending applications with Node.js and GraphQL',
+//     contents: [
+//       { title: 'Node.js', description: 'Executing server side JavaScript and integrating with the frontend'},
+//       { title: 'GraphQL', description: 'Modeling backend APIs with GraphQL and integrating with the frontend' },
+//       { title: 'MongoDB', description: 'Persisting unstructured data using a Non-Relational Database' },
+//       { title: 'Auth', description: 'Authenticating users with passport.js' }
+//     ]
+//   },
+//   {
+//     title: 'Mobile',
+//     icon: <CategoriesJavaScriptSvg />,
+//     description: 'Creating native apps for Android and IOS',
+//     contents: [
+//       { title: 'React Native', description: 'React Native crash course' },
+//       { title: 'Sharing Code', description: 'Sharing code between React and React Native' },
+//       { title: 'Push Notifications', description: 'Interate with the user even when the app is hybernating' },
+//       { title: 'Camera', description: 'Controlling IOS and Android cameras' }
+//     ],
+// 	},
+// 	{
+//     title: "Artificial Intelligence",
+//     icon: <CategoriesJavaScriptSvg />,
+//     description: 'Tackling the big trend in AI today - Deep Learning',
+// 		contents: [
+// 			{ title: 'Tensorflow.js', description: 'Dynamic data computation for deep learning' },
+// 			{ title: 'Sentiment Analysis', description: 'Getting the meaning and the emotion from text sentences' },
+// 			{ title: 'Chatbot', description: 'Using the ??? API' },
+// 			{ title: 'Facial Recognition', description: 'Using the ??? API' }
+// 		]
+// 	},
+// ];
+
+// const Categories = () => {
+//   return (
+//     <CategoriesContainer>
+//       {topics.map((topic, index) => 
+//         <CategoriesItemContainer key={index}>
+//             <CategoriesTitleSvgContainer>
+//               {topic.icon}
+//             </CategoriesTitleSvgContainer>
+//             <CategoriesItemTitleName>
+//               {topic.title}
+//             </CategoriesItemTitleName>
+//             <CategoriesItemTitleDescription>
+//               {topic.description}
+//             </CategoriesItemTitleDescription>
+//           <CategoriesItemContentsContainer>
+//             {topic.contents.map((content, index) => 
+//               <CategoriesItemContentsItem key={index}>
+//                 <CategoriesContentsTitle>
+//                   {content.title}
+//                 </CategoriesContentsTitle>
+//                 <CategoriesContentsDescription>
+//                   {content.description}
+//                 </CategoriesContentsDescription>
+//               </CategoriesItemContentsItem>
+//             )}
+//           </CategoriesItemContentsContainer>
+//         </CategoriesItemContainer>
+//       )}
+//     </CategoriesContainer>
+//   )
+// };
+
+// export default Categories;
+
+import React, { Fragment } from 'react';
 import {
   CategoriesContainer,
   CategoriesItemContainer,
-  CategoriesTitleSvgContainer,
-  CategoriesJavaScriptSvg,
-  CategoriesJavaScriptTmpSvg,
-  CategoriesTypeScriptSvg,
-  CategoriesItemTitleName,
-  CategoriesItemTitleDescription,
-  CategoriesItemContentsContainer,
-  CategoriesItemContentsItem,
-  CategoriesContentsTitle,
-  CategoriesContentsDescription
+  CategoriesIconContainer,
+  CategoriesJavaScriptIcon,
+  CategoriesShieldEchoesIcon,
+  CategoriesTitleText,
+  CategoriesDescriptionText,
+  CategoriesContentsContainer,
+  CategoriesContentItemContainer,
+  CategoriesContentTitleText,
+  CategoriesContentDescriptionText
 } from './Categories.styles';
-// import contents from '../../data/contents';
-import { IoLogoJavascript } from 'react-icons/io';
+import { GiShieldEchoes } from 'react-icons/gi';
+import './Categories.css';
 
-const topics = [
-  {
-    title: 'JavaScript',
-    icon: <CategoriesJavaScriptSvg/>,
-    description: 'Curated tutorials from basic to advanced',
-    contents: [
-      { title: 'Data Structures', description: 'Understanting JavaScript data types, such as booleans, strings, numbers, nulls, arrays and objects'},
-      { title: 'Functions', description: 'Learning how to use function declarations, function expressions and arrow functions'},
-			{ title: 'Conditionals', description: 'Usings if-else, switch and the ternary operator'},
-			{ title: 'Loops', description: 'Iterating over sequences by repetition or by traversing an iterable' },
-			{ title: 'Map', description: 'Processing/Mutating each element of an array according to a certain pattern' },
-			{ title: 'Filter', description: 'Removing elements of an array according to a certain pattern'},
-      { title: 'Reduce', description: 'Processing/Mutating each element of an array and generating an accumulator'},
-      { title: 'Try Catch', description: 'Dealing with erros and exceptions in JavaScript' },
-      { title: 'Closures', description: 'Holding the environment' },
-      { title: 'Generators', description: 'Producing sequential items in steps and gaining performance' },
-      { title: 'Callbacks', description: 'Understanding one of the most fundamental concept from asyncronous programming in JavaScript' },
-			{ title: 'Promises', description: 'Making async calls and avoiding callback hell'},
-			{ title: 'Async Await', description: 'Syntatic sugar for Generator plus Promises'}
-    ]
-  },
-  {
-    title: 'TypeScript',
-    icon: <CategoriesTypeScriptSvg />,
-    description: 'Fundamentals for extending JavaScript',
-    contents: [
-			{ title: 'Types', description: 'Statically type check JavaScript code' },
-			{ title: 'Interfaces', description: 'Creating a blueprint for data consisting of key-value pairs' },
-      { title: 'Generics', description: 'Allowing the usage of multiple types' },
-      { title: 'Decorators', description: 'Injecting extra functionality and metadata' }
-    ]
-  },
-  {
-    title: 'React',
-    icon: <CategoriesJavaScriptSvg />,
-    description: 'Concepts from React, focusing on function components and React Hooks',
-    contents: [
-      { title: 'Rendering', description: 'Rendering on the browser with JXS' },
-      { title: 'Styling', description: 'Creating a fully component oriented app with JSS' },
-			{ title: 'State', description: 'State management and re-rendering components' },
-			{ title: 'Props', description: 'Passing data from parent component to child component' },
-      { title: 'Side Effects', description: 'Dealing with side effects on the component lifecycle' },
-      { title: 'Context', description: 'Creating global variables and methods' },
-      { title: 'Refs', description: 'An escape hatch from react state management' },
-      { title: 'Memoization', description: 'Improving performance by memoizing data and methods' },
-      { title: 'Routing', description: 'Creating Single Page Apps with react-router' },
-      { title: 'Plots', description: 'Creating plots in react with ???' },
-      { title: 'Animation', description: 'Controlling animations with react-spring' },
-      { title: 'Graphics', description: 'Usage of 3D graphics with three.js and react-three-fiber' },
-      { title: 'Videos', description: 'Manipulating videos in react with ???' },
-    ]
-  },
-  {
-    title: 'Backend',
-    icon: <CategoriesJavaScriptSvg />,
-    description: 'Extending applications with Node.js and GraphQL',
-    contents: [
-      { title: 'Node.js', description: 'Executing server side JavaScript and integrating with the frontend'},
-      { title: 'GraphQL', description: 'Modeling backend APIs with GraphQL and integrating with the frontend' },
-      { title: 'MongoDB', description: 'Persisting unstructured data using a Non-Relational Database' },
-      { title: 'Auth', description: 'Authenticating users with passport.js' }
-    ]
-  },
-  {
-    title: 'Mobile',
-    icon: <CategoriesJavaScriptSvg />,
-    description: 'Creating native apps for Android and IOS',
-    contents: [
-      { title: 'React Native', description: 'React Native crash course' },
-      { title: 'Sharing Code', description: 'Sharing code between React and React Native' },
-      { title: 'Push Notifications', description: 'Interate with the user even when the app is hybernating' },
-      { title: 'Camera', description: 'Controlling IOS and Android cameras' }
-    ],
-	},
-	{
-    title: "Artificial Intelligence",
-    icon: <CategoriesJavaScriptSvg />,
-    description: 'Tackling the big trend in AI today - Deep Learning',
-		contents: [
-			{ title: 'Tensorflow.js', description: 'Dynamic data computation for deep learning' },
-			{ title: 'Sentiment Analysis', description: 'Getting the meaning and the emotion from text sentences' },
-			{ title: 'Chatbot', description: 'Using the ??? API' },
-			{ title: 'Facial Recognition', description: 'Using the ??? API' }
-		]
-	},
+const contents = [
+  { title: 'Rendering', description: 'Rendering on the browser with JXS' },
+  { title: 'Styling', description: 'Creating a fully component oriented app with JSS' },
+  { title: 'State', description: 'State management and re-rendering components' },
+  { title: 'Props', description: 'Passing data from parent component to child component' },
+  { title: 'Side Effects', description: 'Dealing with side effects on the component lifecycle' },
+  { title: 'Context', description: 'Creating global variables and methods' },
+  { title: 'Refs', description: 'An escape hatch from react state management' },
+  { title: 'Memoization', description: 'Improving performance by memoizing data and methods' },
+  { title: 'Routing', description: 'Creating Single Page Apps with react-router' },
+  { title: 'Plots', description: 'Creating plots in react with ???' },
+  { title: 'Animation', description: 'Controlling animations with react-spring' },
+  { title: 'Graphics', description: 'Usage of 3D graphics with three.js and react-three-fiber' },
+  { title: 'Videos', description: 'Manipulating videos in react with ???' },
 ];
 
 const Categories = () => {
   return (
+    // <CategoriesIconContainer>
+    //   <CategoriesShieldEchoesIcon />
+    // </CategoriesIconContainer>
     <CategoriesContainer>
-      {topics.map((topic, index) => 
-        <CategoriesItemContainer key={index}>
-            <CategoriesTitleSvgContainer>
-              {topic.icon}
-            </CategoriesTitleSvgContainer>
-            <CategoriesItemTitleName>
-              {topic.title}
-            </CategoriesItemTitleName>
-            <CategoriesItemTitleDescription>
-              {topic.description}
-            </CategoriesItemTitleDescription>
-          <CategoriesItemContentsContainer>
-            {topic.contents.map((content, index) => 
-              <CategoriesItemContentsItem key={index}>
-                <CategoriesContentsTitle>
-                  {content.title}
-                </CategoriesContentsTitle>
-                <CategoriesContentsDescription>
-                  {content.description}
-                </CategoriesContentsDescription>
-              </CategoriesItemContentsItem>
-            )}
-          </CategoriesItemContentsContainer>
-        </CategoriesItemContainer>
-      )}
+      <CategoriesItemContainer>
+        <CategoriesIconContainer>
+          <CategoriesShieldEchoesIcon />
+        </CategoriesIconContainer>
+        <CategoriesTitleText>
+          Artificial Intelligence
+        </CategoriesTitleText>
+        <CategoriesDescriptionText>
+          Concepts from React, focusing on function components and React Hooks
+        </CategoriesDescriptionText>
+        {contents.map((content, index) => 
+          <CategoriesContentItemContainer>
+            <CategoriesContentTitleText>
+              {content.title}
+            </CategoriesContentTitleText>
+            <CategoriesContentDescriptionText>
+              {content.description}
+            </CategoriesContentDescriptionText>
+          </CategoriesContentItemContainer>
+        )}
+      </CategoriesItemContainer>
     </CategoriesContainer>
-  )
-};
+  );
+}
 
 export default Categories;
+
