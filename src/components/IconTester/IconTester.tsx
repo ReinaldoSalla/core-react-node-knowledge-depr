c0f0bb2d7967f6141a265c32342b7ea1e0ab47af
Reinaldo Salla <reinaldo.salla@compasso.com.br>
2020-08-07 19:16:30 -0300

    Added IconTester

---
+++ src/components/IconTester/IconTester.tsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+// import { IconContext }
+// import { BsLayoutTextSidebar } from 'react-icons/bs';
+// import { IoLogoJavascript } from 'react-icons/io';
+
+// const IconProvider = ({
+//   className,
+//   children
+// }) => (
+  
+// )
