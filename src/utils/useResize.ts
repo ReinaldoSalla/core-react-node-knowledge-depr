033d63085d58ab7758ba102484e20c6ba5463d65
Reinaldo Salla <reinaldosalla95@gmail.com>
2020-09-12 09:36:56 -0300

    Comented setWidth in useResize utils until i decide if this hook is a good idea

--- src/utils/useResize.ts
+++ src/utils/useResize.ts
@@ -7,18 +7,18 @@ interface UseResize {
 }
 
 const useResize = (): UseResize => {
-	const [width, setWidth] = useState<number>(window.innerWidth);
+	// const [width, setWidth] = useState<number>(window.innerWidth);
 	const [height, setHeight] = useState<number>(window.innerHeight);
 
 	useEffect(() => {
 		const handleResize = throttle(() => {
 			setHeight(window.innerHeight);
-			setWidth(window.innerWidth);
+			// setWidth(window.innerWidth);
 		}, 100);
 		window.addEventListener('resize', handleResize);
 		return () => window.removeEventListener('resize', handleResize);
 	}, [height]);
-  return { width, height };
+  return { width: 1000, height };
 };
 
 export default useResize;
